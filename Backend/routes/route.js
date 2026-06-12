eb9f9114d233094c1c0e871f2c4cfb20027caf24
unknown <nknandana0@gmail.com>
2026-06-12 12:47:16 +0530

    completed product management application

--- Backend/routes/route.js
+++ Backend/routes/route.js
@@ -18,77 +18,35 @@ router.post('/login', authController.login)
 
 
 // Category
-router.post(
-    '/category',
-    jwtMiddleware,
-    categoryController.addCategory
-)
+router.post('/category', jwtMiddleware, categoryController.addCategory)
 
-router.get(
-    '/category',
-    categoryController.getCategories
-)
+router.get( '/category', categoryController.getCategories)
 
 
 // Subcategory
-router.post(
-    '/subcategory',
-    jwtMiddleware,
-    subCategoryController.addSubCategory
-)
+router.post('/subcategory',jwtMiddleware,subCategoryController.addSubCategory)
 
-router.get(
-    '/subcategory',
-    subCategoryController.getSubCategories
-)
+router.get('/subcategory',subCategoryController.getSubCategories)
 
 
 // Product
-router.post(
-    '/product',
-    jwtMiddleware,
-    upload.array('image', 5),
-    productController.addProduct
-)
+router.post('/product', jwtMiddleware, upload.array('image', 5), productController.addProduct)
 
-router.get(
-    '/products',
-    productController.getProducts
-)
+router.get('/products',productController.getProducts)
 
-router.get(
-    '/product/:id',
-    productController.getSingleProduct
-)
+router.get( '/product/:id',productController.getSingleProduct)
 
-router.put(
-    '/product/:id',
-    jwtMiddleware,
-    productController.updateProduct
-)
+router.put('/product/:id',jwtMiddleware,productController.updateProduct)
 
 
 // Wishlist
-router.post(
-    '/wishlist',
-    jwtMiddleware,
-    wishlistController.addWishlist
-)
+router.post( '/wishlist', jwtMiddleware, wishlistController.addWishlist)
 
-router.get(
-    '/wishlist',
-    jwtMiddleware,
-    wishlistController.getWishlist
-)
+router.get('/wishlist',jwtMiddleware, wishlistController.getWishlist)
 
-router.delete(
-    '/wishlist/:id',
-    jwtMiddleware,
-    wishlistController.removeWishlist
-)
+router.delete( '/wishlist/:id', jwtMiddleware, wishlistController.removeWishlist)
 
-router.get('/', (req, res) => {
-    res.status(200).json('API Working')
+router.get('/', (req, res) => { res.status(200).json('API Working')
 })
 
 module.exports = router
